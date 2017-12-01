a777fec2ecf07e34e19b8fe5e8d4acc79e287186
Darren Rector <darrenrector91@gmail.com>
2017-12-01 03:04:40 -0600

    added weather icons to html

--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ $(document).ready(function() {
     var icon;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
+
         long = position.coords.longitude;
         lat = position.coords.latitude;
+
+        // console.log(long);
+        // console.log(lat);
 
         var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&APPID=33ae0b719c00fb27afcdf1981de535d6"
 
@@ -16,12 +20,14 @@ $(document).ready(function() {
           var weatherType = data.weather[0].description;
           var kTemp = data.main.temp;
           var windspeed = data.wind.speed;
-          var windDirection = degreesToDirection(data.wind.deg);
+          var windDirection = degDirection(data.wind.deg);
           var city = data.name;
           var tempSwap = true;
-          var icon = data.weather[0].icon;
-          console.log(data.weather[0].icon);
-          console.log(data.wind.deg);
+          var icon = "/images/codes/"+(data.weather[0].id+".png");
+          //console.log("/images/codes/"+(data.weather[0].id+".png"));
+          var input = document.getElementById('icon')
+          input.src = icon;
+          //console.log(data.wind.deg);
 
           //Convert Kelvin to Fahrenheit/Celsius
           fTemp = (kTemp * (9 / 5) - 459.67).toFixed(0);
@@ -30,11 +36,7 @@ $(document).ready(function() {
           //Convert meters per second to miles per hour for wind windspeed
           wind = (windspeed * 2.2369).toFixed(0);
 
-          function showIcon(icon) {
-
-          }
-
-          function degreesToDirection(windDirection) {
+          function degDirection(windDirection) {
             for (var i = 1; i <= 360; i++) {
               if (windDirection >= 348.75 && windDirection <= 11.25) {
                 return windDirection = "N"
@@ -70,6 +72,7 @@ $(document).ready(function() {
             }
 
           }
+
           $("#icon").html(icon)
           $("#city").html(city);
           $("#weatherType").html(weatherType);
@@ -83,7 +86,6 @@ $(document).ready(function() {
               tempSwap = false;
             }
           });
-
           $("#windspeed").html(wind + " mph" + " " + windDirection);
         });
       });
